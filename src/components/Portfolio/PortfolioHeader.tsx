f935c3945ac803d656fd53c1036897301528c50a
Kutman <kutman.stel@gmail.com>
2024-01-16 19:11:33 +0100

    adding Dialog for adding transaction

--- src/components/Portfolio/PortfolioHeader.tsx
+++ src/components/Portfolio/PortfolioHeader.tsx
@@ -6,6 +6,16 @@ import { TiArrowSortedUp } from "react-icons/ti";
 import { Switch } from "@/components/ui/switch";
 import { Button } from "@/components/ui/button";
 import { BsThreeDots } from "react-icons/bs";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
 
 export default function PortfolioHeader() {
   return (
@@ -43,7 +53,46 @@ export default function PortfolioHeader() {
           <Label htmlFor="show-charts">Show Charts</Label>
           <Switch id="show-charts" />
         </div>
-        <Button aria-label="Add Transaction">+ Add Transaction</Button>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button aria-label="Add Transaction">+ Add Transaction</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+
         <Button variant="secondary" size="icon" aria-label="Portfolio Settings">
           <BsThreeDots />
         </Button>
