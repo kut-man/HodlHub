4bc358234be5d9f3c1d242211161f814e67a80f5
Kutman <kutman.stel@gmail.com>
2024-01-26 21:13:56 +0100

    fixing bug with overflowing April

--- src/components/Portfolio/PortfolioHeader/DatePicker.tsx
+++ src/components/Portfolio/PortfolioHeader/DatePicker.tsx
@@ -30,7 +30,7 @@ export function DatePicker() {
           {date ? format(date, "PPP") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent side="top" className="w-auto p-0">
         <Calendar
           mode="single"
           selected={date}
