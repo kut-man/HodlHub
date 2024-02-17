038dde57d0b281b054518f22902b74c4c890f4ef
Kutman <kutman.stel@gmail.com>
2024-02-17 18:55:41 +0100

    adding social links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,35 +2,40 @@ import { Flex } from "@tremor/react";
 import Logo from "../Header/Logo";
 import { FaTelegram, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 
+const socialLinks = {
+  LinkedIn: "https://www.linkedin.com/in/kutman-eshenkulov/",
+  GitHub: "https://github.com/kut-man",
+  Telegram: "https://t.me/kut_man",
+  Instagram: "https://www.instagram.com/_.kutman/",
+};
+
 export default function Footer() {
   return (
     <Flex className="border-t py-10 lg:px-32 md:px-12 px-6">
       <Logo />
       <Flex className="hidden sm:flex w-1/2">
-        <a href="https://www.linkedin.com/in/kutman-eshenkulov/" className="text-base font-semibold">
-          LinkedIn
-        </a>
-        <a href="" className="text-base font-semibold">
-          GitHub
-        </a>
-        <a href="" className="text-base font-semibold">
-          Telegram
-        </a>
-        <a href="" className="text-base font-semibold">
-          Instagram
-        </a>
+        {Object.keys(socialLinks).map((value) => (
+          <a
+            target="_blank"
+            key={value}
+            href={socialLinks[value as keyof typeof socialLinks]}
+            className="text-base font-semibold"
+          >
+            {value}
+          </a>
+        ))}
       </Flex>
       <Flex className="sm:hidden w-2/5">
-        <a href="">
+        <a href={socialLinks.LinkedIn}>
           <FaLinkedin size={22} />
         </a>
-        <a href="">
+        <a href={socialLinks.GitHub}>
           <FaGithub size={22} />
         </a>
-        <a href="">
+        <a href={socialLinks.Telegram}>
           <FaTelegram size={22} />
         </a>
-        <a href="">
+        <a href={socialLinks.Instagram}>
           <FaInstagram size={22} />
         </a>
       </Flex>
