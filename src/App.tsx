31f9cc91004067b65b1e02ccd846276e807df4af
Kutman <kutman.stel@gmail.com>
2024-04-25 17:41:10 +0200

    enabling routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import { ThemeProvider } from "./layout/Header/ThemeProvider";
 import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer";
 import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "portfolio",
+    element: <Portfolio />,
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Header />
-      {/* <Portfolio/> */}
-      <Home/>
+      <RouterProvider router={router} />
       <Footer />
     </ThemeProvider>
   );
