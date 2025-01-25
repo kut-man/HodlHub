3b67bd56f2582cdaf9607327732b130f0f739add
Kutman <kutman.stel@gmail.com>
2025-01-25 21:14:41 +0100

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://hodlhubspring.onrender.com';
 export const LOGOUT_URL = `${BASE_URL}/logout`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const REGISTER_URL = `${BASE_URL}/auth/register`;
