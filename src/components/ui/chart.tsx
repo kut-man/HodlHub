85d4fcc5625d37ba6922a630349cb83d53d99787
kut-man <kutman.stel@gmail.com>
2026-06-07 19:12:58 +0200

    Turn off erasableSyntaxOnly

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -220,7 +220,7 @@ function ChartTooltipContent({
 
           return (
             <div
-              key={item.dataKey}
+              key={index}
               className={cn(
                 "flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground",
                 indicator === "dot" && "items-center"
